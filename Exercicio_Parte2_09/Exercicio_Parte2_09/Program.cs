5db7cc623657895c566d609fde75817f68f26b98
Alberto <albertomaronezio@hotmai.com>
2019-05-29 15:12:09 -0300

    Exercicio_Parte2_09

---
+++ Exercicio_Parte2_09/Exercicio_Parte2_09/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Parte2_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroA = 0;
+            int numeroB = 0;
+
+            Console.Write("Digite um valor para A: ");
+            numeroA = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um valor para B: ");
+            numeroB = int.Parse(Console.ReadLine());
+
+            if (numeroA == numeroB)
+            {
+                Console.WriteLine("A igual a B");
+            }
+
+            else if (numeroA > numeroB)
+            {
+                Console.WriteLine("A maior do que B");
+            }
+
+            else
+            {
+                Console.WriteLine("B maior do que A");
+            }
+        }
+    }
+}
